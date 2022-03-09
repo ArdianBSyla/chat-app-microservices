59634d2620f0ccc346fbba6a638ac8de21a5a661
Ardian <ardianbsyla@gmail.com>
2022-03-09 16:23:58 +0100

    Add options for web socket gateway such as port, namespace etc

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -2,7 +2,7 @@ import { Logger } from '@nestjs/common';
 import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway(3001, { path: '/websockets', serveClient: true, namespace: '/', cors: true })
 export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
 
   @WebSocketServer() wss: Server;
@@ -24,8 +24,5 @@ export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGateway
   @SubscribeMessage('msgToServer')
   handleMessage(client: Socket, text: string): void {
     this.wss.emit('msgToClient', text)
-    // return { event: 'msgToClient', data: text };
   }
-
-
 }
